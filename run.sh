f0876c1d09c1fa6f92a1e20bd6b550803124f0cb
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2025-05-16 12:01:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 times="1 2 3"
 for t in $times
 do
-    for ws in $window_size
-    do
-        python train.py --wp 2 --wf 2 --fp16 --n_layers 2 --weight_decay 0.01 > log/ablation/RGCN_${t}.log
-    done
+    python train.py --wp 2 --wf 2 --fp16 --n_layers 2 --weight_decay 0.01 > log/ablation/RGCN_${t}.log
 done
