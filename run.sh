9f9d6c054144aacf3136f99fd9a1c21944c7a992
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2025-05-16 15:25:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 times="1 2 3"
+python emo_emb.py
 for t in $times
 do
-    python train.py --wp 2 --wf 2 --fp16 --n_layers 2 --weight_decay 0.01 > log/ablation/RGCN_${t}.log
+    python train.py --wp 2 --wf 2 --fp16 --n_layers 2 --weight_decay 0.01 > "log/ablation/RGCN_${t}.log"
 done
